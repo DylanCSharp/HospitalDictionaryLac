730b62d4d48a3ab6578db8609080c99eab1d9c76
dylan <dylan@DYLANPC>
2021-10-09 11:02:40 +0200

    Add project files.

---
+++ HospitalDictionaryLac/Patient.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalDictionaryLac
+{
+
+
+    public class Patient
+    {
+        #region Properties
+        public string Name { get; set; }
+        public DietEnum.Diet Diet { get; set; }
+        public double Ward { get; set; }
+        public List<string> Allergies { get; set; }
+        #endregion
+
+        public static readonly Dictionary<int, Patient> dictionaryPatients = new()
+        {
+            { 1, new Patient { Name = "Dylan", Diet = DietEnum.Diet.None, Ward = 3, Allergies = new List<string> { "None" } } },
+            { 2, new Patient { Name = "Emily", Diet = DietEnum.Diet.Vegetarian, Ward = 4, Allergies = new List<string> { "Tomato", "Peanuts" } } },
+            { 3, new Patient { Name = "Andrew", Diet = DietEnum.Diet.None, Ward = 2, Allergies = new List<string> { "None" } } },
+            { 4, new Patient { Name = "Wikus", Diet = DietEnum.Diet.None, Ward = 3, Allergies = new List<string> { "Popcorn", "Lettuce", "Peanuts" } } },
+            { 5, new Patient { Name = "Simon", Diet = DietEnum.Diet.Vegan, Ward = 3, Allergies = new List<string> { "Meat" } } }
+        };
+
+        public static Dictionary<int, Patient> GetPatientDetails()
+        {
+            return dictionaryPatients;
+        }
+
+        public List<string> GetAllergies()
+        {
+            return Allergies;
+        }
+
+        public void SetAllergies(List<string> value)
+        {
+            Allergies = value;
+        }
+    }
+}
---
+++ HospitalDictionaryLac/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HospitalDictionaryLac
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PATIENTS THAT ARE VEGETARIAN");
+            foreach (var item in Patient.GetPatientDetails())
+            {
+                if (item.Value.Diet == DietEnum.Diet.Vegetarian)
+                {
+                    Console.WriteLine($"ID: {item.Key}\nNAME: {item.Value.Name}\nDIET: {item.Value.Diet}");
+                    Console.WriteLine("List of Allergies: ");
+                    if (item.Value.Allergies.Count > 1)
+                    {
+                        foreach (var allergy in item.Value.Allergies)
+                        {
+                            Console.WriteLine($"{allergy}");
+                        }
+                        Console.WriteLine("");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{item.Value.Allergies[0]}\n");
+                    }
+                }
+            }
+
+            Console.WriteLine("PATIENTS THAT ARE ALLERGIC TO PEANUTS");
+            foreach (var item in Patient.GetPatientDetails())
+            {
+                if (item.Value.Allergies.Contains("Peanuts"))
+                {
+                    Console.WriteLine($"ID: {item.Key}\nNAME: {item.Value.Name}\nDIET: {item.Value.Diet}");
+                    Console.WriteLine("List of Allergies: ");
+                    if (item.Value.Allergies.Count > 1)
+                    {
+                        foreach (var allergy in item.Value.Allergies)
+                        {
+                            Console.WriteLine($"{allergy}");
+                        }
+                        Console.WriteLine("");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{item.Value.Allergies[0]}\n");
+                    }
+                }
+            }
+        }
+    }
+}
